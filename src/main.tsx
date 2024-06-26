6c987da707769fdc98fecbf64c4bb65fecbc92e2
Simanta Paul <simanta.paul@nexgencloud.com>
2024-06-26 15:56:40 +0600

    move config file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Amplify } from 'aws-amplify';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
 
-import config from './aws-exports';
-import App from './App.tsx';
-import './styles/globals.scss';
+import config from "configs/aws-config.ts";
+
+import App from "./App.tsx";
+import "./styles/globals.scss";
 
 Amplify.configure(config);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
