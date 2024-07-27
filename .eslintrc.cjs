b5484169e3ddc2d335e35dbe2fe1a56f0e5375bd
Nazmul Haque <bpinazmul18@gmail.com>
2024-07-27 11:06:56 +0600

    Fix: No unused var config issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "no-unused-vars": [false],
+    // "no-unused-vars": [false],
+    "no-unused-vars": 0,
   },
 };
