f352556b60fc38405e1b2d3d2fa80c6ad97d1b52
Nazmul Haque <bpinazmul18@gmail.com>
2024-08-28 11:27:44 +0600

    Updated: Avatar component status

--- src/components/UI/components/Avatar/index.tsx
+++ src/components/UI/components/Avatar/index.tsx
@@ -1,3 +1,20 @@
+// import {
+//   Avatar as AvatarField,
+//   AvatarProps,
+//   ElementProps,
+// } from "@mantine/core";
+
+// interface AvatarFieldProps
+//   extends AvatarProps,
+//     ElementProps<"button", keyof AvatarProps> {}
+
+// const Avatar = ({ ...rest }: AvatarFieldProps) => {
+//   return <AvatarField component="button" {...rest} />;
+// };
+
+// export default Avatar;
+
+
 import {
   Avatar as AvatarField,
   AvatarProps,
@@ -6,10 +23,24 @@ import {
 
 interface AvatarFieldProps
   extends AvatarProps,
-    ElementProps<"button", keyof AvatarProps> {}
+  ElementProps<"button", keyof AvatarProps> {
+  active?: boolean;
+}
 
-const Avatar = ({ ...rest }: AvatarFieldProps) => {
-  return <AvatarField component="button" {...rest} />;
+const Avatar = ({ active = false, style, ...rest }: AvatarFieldProps) => {
+  const activeStyles = active
+    ? {
+      border: '0.75px solid #000000',
+    }
+    : {};
+
+  return (
+    <AvatarField
+      component="button"
+      style={{ ...style, ...activeStyles }}
+      {...rest}
+    />
+  );
 };
 
 export default Avatar;
