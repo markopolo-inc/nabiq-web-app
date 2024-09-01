b66183cab9720da5c99a5ef52001a133c251498c
Nazmul Haque <bpinazmul18@gmail.com>
2024-09-01 13:47:08 +0600

    Remove: Unused code

--- src/components/Features/Sidebar/TopMenu/index.tsx
+++ src/components/Features/Sidebar/TopMenu/index.tsx
@@ -1,52 +1,8 @@
-// import { Avatar, Menu, useDisclosure } from "@nabiq-ui"
-
-// const TopMenu = () => {
-//     const [opened, { toggle, close }] = useDisclosure(false);
-
-//     return (
-//         <Menu shadow="md" width={240}>
-//             <Menu.Target>
-//                 <Avatar
-//                     src="img.png"
-//                     alt="It's me"
-//                     onClick={toggle}
-//                     className="border-4 border-[#98A2B324]"
-//                     size="lg"
-//                 />
-//             </Menu.Target>
-
-//             <Menu.Dropdown>
-//                 <Menu.Label>
-//                     <div className="flex gap-3 py-3 px-4 ">
-//                         <Avatar
-//                             src="img.png"
-//                             alt="It's me"
-//                             active
-//                             size="md"
-//                         />
-
-//                         <div className="flex flex-col items-start font-sans text-sm font-semibold leading-5 text-gray-700">
-//                             <div>John Doe</div>
-//                             <div className="font-normal text-gray-600">johndoe@gmail.com</div>
-//                         </div>
-//                     </div>
-//                 </Menu.Label>
-
-//                 <Menu.Item component="a" href="https://mantine.dev">
-//                     Mantine website
-//                 </Menu.Item>
-//             </Menu.Dropdown>
-//         </Menu>
-//     )
-// }
-
-// export default TopMenu
-
 import { Menu, UnstyledButton, Image, Avatar, Divider, useGetColors } from "@nabiq-ui"
 import { FiHelpCircle, FiSettings01, Logout01 } from "@nabiq-icons"
 import { rem } from "@mantine/core";
 
-function TopMenu() {
+const TopMenu = () => {
     const { gray500 } = useGetColors();
 
     return (
