841b737ddecf555ec624eee99b1dae57cd464192
Nazmul Haque <bpinazmul18@gmail.com>
2024-09-23 12:09:55 +0600

    Fixed Button spacing and Primary button

--- src/pages/TestButton/index.tsx
+++ src/pages/TestButton/index.tsx
@@ -1,8 +1,101 @@
+import { FiCircle } from '@nabiq-icons';
+import { Button, Group, Stack } from '@nabiq-ui';
+
 const TestButton = () => {
   return (
-    <>
-      <div>Hello button</div>
-    </>
+    <div className='p-10'>
+      <Stack gap={140}>
+        <Stack gap={16}>
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button trailingIcon={<FiCircle size={20} />} leadingIcon={<FiCircle size={20} />}>
+                Button CTA
+              </Button>
+              <Button
+                size='md'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+              >
+                Button CTA
+              </Button>
+              <Button
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='md'>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='lg'>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button>Button CTA</Button>
+              <Button size='md'>Button CTA</Button>
+              <Button
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='md'>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='lg'>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button disabled>Button CTA</Button>
+              <Button size='md' disabled>
+                Button CTA
+              </Button>
+              <Button
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+                disabled
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button disabled>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='md' disabled>
+                <FiCircle size={20} />
+              </Button>
+              <Button size='lg' disabled>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+        </Stack>
+      </Stack>
+    </div>
   );
 };
 
