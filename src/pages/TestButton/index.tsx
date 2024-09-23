b341d5fa8b30bd34864be8988e36a4c6811b2883
Nazmul Haque <bpinazmul18@gmail.com>
2024-09-23 15:04:47 +0600

    Added Link button in test page

--- src/pages/TestButton/index.tsx
+++ src/pages/TestButton/index.tsx
@@ -608,6 +608,130 @@ const TestButton = () => {
             </Group>
           </Stack>
         </Stack>
+        <Stack gap={16}>
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button
+                variant='link'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                size='md'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button variant='link'>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='md'>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='lg'>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button
+                variant='link'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+                size='md'
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button variant='link'>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='md'>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='lg'>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+
+          <Stack gap={180} className='flex flex-row'>
+            <Group gap={32}>
+              <Button
+                variant='link'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+                disabled
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                trailingIcon={<FiCircle size={20} />}
+                leadingIcon={<FiCircle size={20} />}
+                size='md'
+                disabled
+              >
+                Button CTA
+              </Button>
+              <Button
+                variant='link'
+                size='lg'
+                trailingIcon={<FiCircle size={24} />}
+                leadingIcon={<FiCircle size={24} />}
+                disabled
+              >
+                Button CTA
+              </Button>
+            </Group>
+
+            <Group gap={32}>
+              <Button variant='link' disabled>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='md' disabled>
+                <FiCircle size={20} />
+              </Button>
+              <Button variant='link' size='lg' disabled>
+                <FiCircle size={24} />
+              </Button>
+            </Group>
+          </Stack>
+        </Stack>
       </Stack>
     </div>
   );
