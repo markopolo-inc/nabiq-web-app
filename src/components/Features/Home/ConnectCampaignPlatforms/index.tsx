e7f30e99dfba6d3db13c49204275cf593b3fcd4b
Nazmul Haque <bpinazmul18@gmail.com>
2024-09-29 15:14:39 +0600

    Implement platform icon, sub title and button link

--- src/components/Features/Home/ConnectCampaignPlatforms/index.tsx
+++ src/components/Features/Home/ConnectCampaignPlatforms/index.tsx
@@ -1,9 +1,9 @@
-import { FiHexagon01 } from '@nabiq-icons';
+import { FiHexagon01, FiPlatformIcon } from '@nabiq-icons';
 import { Button } from '@nabiq-ui';
-import { useNavigate } from 'react-router-dom';
 
 const ConnectCampaignPlatforms = () => {
-  const navigate = useNavigate();
+  const apps = ['facebook', 'google'];
+
   return (
     <div className='bg-white rounded-xl p-8 shadow-lg'>
       <div className='flex gap-3 flex-nowrap'>
@@ -14,15 +14,26 @@ const ConnectCampaignPlatforms = () => {
         <div className='flex flex-col gap-16'>
           <div className='flex flex-col gap-1'>
             <p className='text-gray-900 text-lg font-semibold'>Connect campaign platforms</p>
+            <p className='text-gray-600 text-md font-medium'>Connect campaign with Markopolo.ai</p>
             <p className='text-gray-600 text-sm font-normal'>
               Connect and import data from ads you have run on facebook ads, google ads and linkedin
               ads.
             </p>
           </div>
-          <div className='flex gap-3'>
-            <Button variant='secondary' onClick={() => navigate('/campaigns')}>
+          <div className='flex gap-3 justify-between items-center'>
+            <Button
+              variant='secondary'
+              onClick={() => window.open('https://app.markopolo.ai/brand/dashboard', '_blank')}
+            >
               Connect
             </Button>
+            <div className='flex gap-4'>
+              {apps.map((item) => (
+                <div key={item}>
+                  <FiPlatformIcon platform={item} size={20} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
