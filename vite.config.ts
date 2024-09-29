57edac6a6f2714b3ca917c75d12cefcb2d852df7
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2024-09-29 16:10:14 +0600

    Fix/mad 2018/onboarding issues (#29)
    * Fix redirectSigninUrl and redirectSignoutUrl issue
    
    * Fix Localhost ssl issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: "window",
+    global: 'window',
   },
   plugins: [react(), viteTsconfigPaths()],
   build: {
-    outDir: "build",
+    outDir: 'build',
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, './localhost.pem')),
+    },
+    // Make sure the server is accessible over the local network
+    host: '0.0.0.0',
   },
 });
