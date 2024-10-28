c42345b6ac596a956b14b1725923d448de857cb9
Nazmul Haque <bpinazmul18@gmail.com>
2024-10-28 12:18:24 +0600

    Working on Checkbox Icon

--- src/components/UI/components/Checkbox/index.tsx
+++ src/components/UI/components/Checkbox/index.tsx
@@ -14,42 +14,85 @@ const Checkbox = ({
   ...rest
 }: TCheckboxProps) => {
   const isDisabled = disabled;
-  // const CheckboxIcon: TCheckboxProps['icon'] = ({ indeterminate, ...others }) =>
-  //   indeterminate ? (
-  //     <svg
-  //       width='14'
-  //       height='14'
-  //       viewBox='0 0 14 14'
-  //       fill='none'
-  //       xmlns='http://www.w3.org/2000/svg'
-  //       {...others}
-  //     >
-  //       <path
-  //         d='M2.9165 7H11.0832'
-  //         stroke='white'
-  //         stroke-width='2'
-  //         stroke-linecap='round'
-  //         stroke-linejoin='round'
-  //       />
-  //     </svg>
-  //   ) : (
-  //     <svg
-  //       width='14'
-  //       height='14'
-  //       viewBox='0 0 14 14'
-  //       fill='none'
-  //       xmlns='http://www.w3.org/2000/svg'
-  //       {...others}
-  //     >
-  //       <path
-  //         d='M11.6668 3.5L5.25016 9.91667L2.3335 7'
-  //         stroke='white'
-  //         stroke-width='2'
-  //         stroke-linecap='round'
-  //         stroke-linejoin='round'
-  //       />
-  //     </svg>
-  //   );
+  const CheckboxIcon: TCheckboxProps['icon'] = ({ indeterminate, ..._rest }) => {
+    const checkboxSize = size === 'sm' ? 12 : 14;
+    const radioSize = size === 'sm' ? 6 : 8;
+
+    const checkboxOthers = { width: checkboxSize, height: checkboxSize, ..._rest };
+    const radioOthers = { width: radioSize, height: radioSize, ..._rest };
+
+    if (variant === 'checkbox' && indeterminate && !isDisabled)
+      // show minus
+      return (
+        <svg viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...checkboxOthers}>
+          <path
+            d='M2.5 6H9.5'
+            stroke='white'
+            strokeWidth='1.66666'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      );
+
+    if (variant === 'checkbox' && indeterminate && isDisabled)
+      // show minus by disabled
+      return (
+        <svg viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...checkboxOthers}>
+          <path
+            d='M2.5 6H9.5'
+            stroke='#CDD5DF'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      );
+
+    if (variant === 'checkbox' && !isDisabled)
+      // show tik
+      return (
+        <svg viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...checkboxOthers}>
+          <path
+            d='M10 3L4.5 8.5L2 6'
+            stroke='white'
+            strokeWidth='1.6666'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      );
+    if (variant === 'checkbox' && isDisabled)
+      // show tik by disabled
+      return (
+        <svg viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...checkboxOthers}>
+          <path
+            d='M10 3L4.5 8.5L2 6'
+            stroke='#CDD5DF'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      );
+
+    if (variant === 'radio' && !isDisabled)
+      // show circle
+      return (
+        <svg viewBox='0 0 6 6' fill='none' xmlns='http://www.w3.org/2000/svg' {...radioOthers}>
+          <rect width='6' height='6' rx='3' fill='white' />
+        </svg>
+      );
+
+    if (variant === 'radio' && isDisabled)
+      // show circle by disabled
+      return (
+        <svg viewBox='0 0 6 6' fill='none' xmlns='http://www.w3.org/2000/svg' {...radioOthers}>
+          <rect width='6' height='6' rx='3' fill='#CDD5DF' />
+        </svg>
+      );
+  };
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (onChange) {
       onChange(e);
@@ -67,7 +110,7 @@ const Checkbox = ({
       onChange={(e) => handleChange(e)}
       size={size}
       radius={variant === 'radio' ? 'xl' : 'sm'}
-      // icon={CheckboxIcon}
+      icon={CheckboxIcon}
       {...rest}
     />
   );
