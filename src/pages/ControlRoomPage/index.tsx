63ec2f3d99372f2344abd369e8228439c73b06f0
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2024-11-21 15:15:36 +0600

    Added: Control room empty screen (#59)

--- src/pages/ControlRoomPage/index.tsx
+++ src/pages/ControlRoomPage/index.tsx
@@ -1,23 +1,36 @@
-import { FiHelpCircle } from '@nabiq-icons';
-import { Button, ContentLoader, Group, Stack } from '@nabiq-ui';
+import { FiHelpCircle, FiPlus, FiSliders01 } from '@nabiq-icons';
+import { Button, ContentLoader, Group, Stack, useGetColors } from '@nabiq-ui';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { CampaignGoalModal } from 'src/components/modules/campaigns';
 import { ConfigCard, LearnMoreControlRoomModal } from 'src/components/modules/control-room';
 import { IControlRoomConfig } from 'src/interfaces/controlRoom.interface';
+import { resetCampaign } from 'src/store/campaign/campaignSlice.ts';
 import { useGetConfigsQuery } from 'src/store/controlRoom/controlRoom.api.ts';
 
 const ControlRoom = () => {
+  const dispatch = useDispatch();
+  const { gray700 } = useGetColors();
   const [showLearnMoreControlRoomModal, setLearnMoreControlRoomModal] = useState<boolean>(false);
+  const [showGoalModal, setShowGoalModal] = useState<boolean>(false);
 
   const { data, isLoading } = useGetConfigsQuery({ limit: 10, page: 1 });
   const configs: IControlRoomConfig[] = data?.data?.configs || [];
 
+  const handleCreateCampaign = () => {
+    dispatch(resetCampaign());
+    setShowGoalModal(true);
+  };
+
   return (
     <>
+      <CampaignGoalModal showModal={showGoalModal} setShowModal={setShowGoalModal} />
+
       <LearnMoreControlRoomModal
         showModal={showLearnMoreControlRoomModal}
         setShowModal={setLearnMoreControlRoomModal}
       />
-      <Stack gap={64}>
+      <Stack gap={configs.length === 0 ? 160 : 64}>
         <Group justify='space-between'>
           <Stack gap={4}>
             <p className='text-gray-900 text-3xl font-semibold'>Control room</p>
@@ -34,9 +47,30 @@ const ControlRoom = () => {
           </Button>
         </Group>
 
-        {isLoading ? (
-          <ContentLoader />
-        ) : (
+        {isLoading && <ContentLoader />}
+
+        {configs.length === 0 && (
+          <Stack align='center' gap={24}>
+            <Stack gap={16} align='center'>
+              <div className='border border-gray-200 bg-white shadow-xs p-3 rounded-[10px]'>
+                <FiSliders01 size={24} color={gray700} />
+              </div>
+              <Stack gap={4} align='center'>
+                <p className='text-gray-900 text-3xl font-semibold'>
+                  No campaigns to show at the moment.
+                </p>
+                <p className='text-gray-600 text-base font-normal'>
+                  Create a campaign to see updates here.
+                </p>
+              </Stack>
+            </Stack>
+            <Button leadingIcon={<FiPlus size={20} color='white' />} onClick={handleCreateCampaign}>
+              Create campaign
+            </Button>
+          </Stack>
+        )}
+
+        {!!configs.length && (
           <Stack align='center'>
             <Stack gap={32}>
               {configs.map((item, idx) => (
