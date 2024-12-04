631167e5a0b25c1a42093f6add3dfe42636d37b9
Simanta Paul <simanta.paul.26@gmail.com>
2024-12-04 14:35:34 +0600

    feat: increase gap

--- src/components/UI/components/Card/index.tsx
+++ src/components/UI/components/Card/index.tsx
@@ -10,7 +10,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        'rounded-xl border border-gray-200 p-6 shadow-sm min-h-60 flex flex-col justify-between gap-8',
+        'rounded-xl border border-gray-200 p-6 shadow-sm min-h-60 flex flex-col justify-between gap-12',
         className,
       )}
     >
