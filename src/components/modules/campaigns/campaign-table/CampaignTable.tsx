a5bf8842a9a36880f554ac58c750b58653af5a26
Nazmul Haque <bpinazmul18@gmail.com>
2024-12-08 22:15:55 +0600

    Added: Confirm modal

--- src/components/modules/campaigns/campaign-table/CampaignTable.tsx
+++ src/components/modules/campaigns/campaign-table/CampaignTable.tsx
@@ -9,6 +9,7 @@ import {
 import {
   Badge,
   Button,
+  ConfirmationModal,
   Group,
   Menu,
   OptionTabs,
@@ -27,7 +28,7 @@ import {
 } from '@nabiq-ui';
 import { capitalize } from 'lodash';
 import moment from 'moment-timezone';
-import { useMemo, useState } from 'react';
+import { Fragment, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ICampaignItem } from 'src/interfaces/modules/campaign';
 import { useDeleteCampaignConfigMutation } from 'src/store/campaign/campaignApi';
@@ -53,6 +54,8 @@ const colorMap = {
 };
 
 export const CampaignTable = ({ list, refetch }) => {
+  const [showDisconnectModal, setShowDisconnectModal] = useState<boolean>(false);
+  const [campaignId, setCampaignId] = useState<null | string>(null);
   const navigate = useNavigate();
   // const dispatchCampaign = useCampaignDispatch();
   const [deleteConfig, { isLoading }] = useDeleteCampaignConfigMutation();
@@ -90,11 +93,16 @@ export const CampaignTable = ({ list, refetch }) => {
   //   navigate('/campaigns/create-campaign');
   // };
 
-  const handleDeleteCampaign = async ({ campaignId }) => {
-    if (!window.confirm('Are you want to delete this campaign?')) return;
+  const handleDeleteCampaign = async () => {
+    setShowDisconnectModal(true);
+  };
+
+  const handleDisconnect = async () => {
+    if (!campaignId) return;
 
     const res = await deleteConfig(campaignId).unwrap();
     if (res.success) {
+      setShowDisconnectModal(false);
       refetch();
     }
   };
@@ -139,129 +147,140 @@ export const CampaignTable = ({ list, refetch }) => {
   );
 
   return (
-    <Table banner={banner} withBanner>
-      {filteredList.length > 0 && (
-        <TableHead>
-          <TableRow>
-            {CAMPAIGN_TABLE_HEADERS.map((item, idx) => (
-              <Th key={idx}>
-                <div className='flex items-center gap-1'>
-                  <div className='text-xs font-medium text-gray-600'>{item}</div>
-                </div>
-              </Th>
-            ))}
-          </TableRow>
-        </TableHead>
-      )}
-
-      <TableBody>
-        {filteredList.length === 0 ? (
-          <TableRow>
-            <Td className='py-10 px-8' colSpan={7}>
-              <Stack align='center' gap={16}>
-                {list.length !== 0 ? (
-                  <div className='border border-gray-200 shadow-xs rounded-[10px] p-[11px]'>
-                    <FiSearchLg size={24} color='#364152' />
+    <Fragment>
+      <Table banner={banner} withBanner>
+        {filteredList.length > 0 && (
+          <TableHead>
+            <TableRow>
+              {CAMPAIGN_TABLE_HEADERS.map((item, idx) => (
+                <Th key={idx}>
+                  <div className='flex items-center gap-1'>
+                    <div className='text-xs font-medium text-gray-600'>{item}</div>
                   </div>
-                ) : (
-                  <></>
-                )}
-                <Stack align='center' gap={4}>
-                  <p className='text-gray-900 font-semibold text-base'>
-                    No campaigns {list.length === 0 ? 'created yet' : 'found!'}
-                  </p>
-                  <p className='text-gray-600 text-sm'>
-                    Your {list.length === 0 ? 'created' : 'filtered'} campaigns will show up here.
-                  </p>
-                </Stack>
-              </Stack>
-            </Td>
-          </TableRow>
-        ) : (
-          filteredList.map((item, idx) => (
-            <TableRow key={idx} className='odd:!bg-gray-50 even:!bg-white hover:!bg-primary-100'>
-              <Td className='py-4 px-6'>
-                <Stack align='left' gap={4}>
-                  <Switch checked={idx % 2 === 0} />
-                </Stack>
-              </Td>
-              <Td className='py-4 px-6'>
-                <Stack align='left' gap={4}>
-                  <div className='text-sm font-medium text-gray-900'>{item.name}</div>
-                  <Tooltip label={item.details}>
-                    <div className='text-xs font-medium text-gray-600 truncate w-[234px]'>
-                      {item.details}
+                </Th>
+              ))}
+            </TableRow>
+          </TableHead>
+        )}
+
+        <TableBody>
+          {filteredList.length === 0 ? (
+            <TableRow>
+              <Td className='py-10 px-8' colSpan={7}>
+                <Stack align='center' gap={16}>
+                  {list.length !== 0 ? (
+                    <div className='border border-gray-200 shadow-xs rounded-[10px] p-[11px]'>
+                      <FiSearchLg size={24} color='#364152' />
                     </div>
-                  </Tooltip>
-                </Stack>
-              </Td>
-
-              <Td className='py-4 px-6'>
-                <Stack align='left' gap={4} className='flex-row'>
-                  <div className='p-2'>
-                    <FiMail01 size={20} color='#475467' />
-                  </div>
-                  <div className='p-2'>
-                    <FiMessageDotCircle size={20} color='#475467' />
-                  </div>
-                </Stack>
-              </Td>
-
-              <Td className='py-4 px-6'>
-                <Stack align='left'>1119/13000</Stack>
-              </Td>
-
-              <Td className='py-4 px-6'>
-                <Stack align='left' gap={4}>
-                  {moment(item.createdAt).format('MMM DD, YYYY [at] hh:mm a')}
-                </Stack>
-              </Td>
-
-              <Td className='py-4 px-6'>
-                <Stack align='left' gap={4}>
-                  <Badge color={(colorMap?.[item.status] as 'success') || 'success'}>
-                    {capitalize('High')}
-                  </Badge>
-                </Stack>
-              </Td>
-
-              <Td className='py-4 px-6'>
-                <Stack align='center' className='flex-row' gap={12}>
-                  <Button
-                    variant='secondary-black'
-                    onClick={() => navigate(`/campaigns/details/${item.resourceId}`)}
-                  >
-                    Details
-                  </Button>
-                  <Button variant='secondary'>
-                    <FiBarChart04 color='#364152' size={20} />
-                  </Button>
-
-                  <Menu width={240} position='bottom-end'>
-                    <Menu.Target>
-                      <UnstyledButton>
-                        <FiDotsVertical size={20} color='#475467' style={{ cursor: 'pointer' }} />
-                      </UnstyledButton>
-                    </Menu.Target>
-
-                    <Menu.Dropdown className='!p-0 !rounded-2xl !shadown-none !border-none'>
-                      <Button
-                        variant='secondary'
-                        fullWidth
-                        onClick={() => handleDeleteCampaign({ campaignId: item.resourceId })}
-                        disabled={isLoading}
-                      >
-                        <FiTrash size={20} color='#F04438' />
-                        Delete
-                      </Button>
-                    </Menu.Dropdown>
-                  </Menu>
+                  ) : (
+                    <></>
+                  )}
+                  <Stack align='center' gap={4}>
+                    <p className='text-gray-900 font-semibold text-base'>
+                      No campaigns {list.length === 0 ? 'created yet' : 'found!'}
+                    </p>
+                    <p className='text-gray-600 text-sm'>
+                      Your {list.length === 0 ? 'created' : 'filtered'} campaigns will show up here.
+                    </p>
+                  </Stack>
                 </Stack>
               </Td>
             </TableRow>
-          ))
-        )}
-      </TableBody>
-    </Table>
+          ) : (
+            filteredList.map((item, idx) => (
+              <TableRow key={idx} className='odd:!bg-gray-50 even:!bg-white hover:!bg-primary-100'>
+                <Td className='py-4 px-6'>
+                  <Stack align='left' gap={4}>
+                    <Switch checked={idx % 2 === 0} />
+                  </Stack>
+                </Td>
+                <Td className='py-4 px-6'>
+                  <Stack align='left' gap={4}>
+                    <div className='text-sm font-medium text-gray-900'>{item.name}</div>
+                    <Tooltip label={item.details}>
+                      <div className='text-xs font-medium text-gray-600 truncate w-[234px]'>
+                        {item.details}
+                      </div>
+                    </Tooltip>
+                  </Stack>
+                </Td>
+
+                <Td className='py-4 px-6'>
+                  <Stack align='left' gap={4} className='flex-row'>
+                    <div className='p-2'>
+                      <FiMail01 size={20} color='#475467' />
+                    </div>
+                    <div className='p-2'>
+                      <FiMessageDotCircle size={20} color='#475467' />
+                    </div>
+                  </Stack>
+                </Td>
+
+                <Td className='py-4 px-6'>
+                  <Stack align='left'>1119/13000</Stack>
+                </Td>
+
+                <Td className='py-4 px-6'>
+                  <Stack align='left' gap={4}>
+                    {moment(item.createdAt).format('MMM DD, YYYY [at] hh:mm a')}
+                  </Stack>
+                </Td>
+
+                <Td className='py-4 px-6'>
+                  <Stack align='left' gap={4}>
+                    <Badge color={(colorMap?.[item.status] as 'success') || 'success'}>
+                      {capitalize('High')}
+                    </Badge>
+                  </Stack>
+                </Td>
+
+                <Td className='py-4 px-6'>
+                  <Stack align='center' className='flex-row' gap={12}>
+                    <Button
+                      variant='secondary-black'
+                      onClick={() => navigate(`/campaigns/details/${item.resourceId}`)}
+                    >
+                      Details
+                    </Button>
+                    <Button variant='secondary'>
+                      <FiBarChart04 color='#364152' size={20} />
+                    </Button>
+
+                    <Menu width={240} position='bottom-end'>
+                      <Menu.Target>
+                        <UnstyledButton>
+                          <FiDotsVertical size={20} color='#475467' style={{ cursor: 'pointer' }} />
+                        </UnstyledButton>
+                      </Menu.Target>
+
+                      <Menu.Dropdown className='!p-0 !rounded-2xl !shadown-none !border-none'>
+                        <Button
+                          variant='secondary'
+                          fullWidth
+                          onClick={() => {
+                            setCampaignId(item.resourceId);
+                            handleDeleteCampaign();
+                          }}
+                          disabled={isLoading}
+                        >
+                          <FiTrash size={20} color='#F04438' />
+                          Delete
+                        </Button>
+                      </Menu.Dropdown>
+                    </Menu>
+                  </Stack>
+                </Td>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+      <ConfirmationModal
+        title='Are you sure you want to disconnect this platform?'
+        showModal={showDisconnectModal}
+        setShowModal={setShowDisconnectModal}
+        onConfirm={handleDisconnect}
+      />
+    </Fragment>
   );
 };
