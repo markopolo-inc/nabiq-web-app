e4e939849e4eb9d64e7db407a7160b3827f53177
Nazmul Haque <bpinazmul18@gmail.com>
2024-12-16 00:17:50 +0600

    Fixed: width

--- src/components/modules/home/components/cards/ConnectFirstMarkTagCard/index.tsx
+++ src/components/modules/home/components/cards/ConnectFirstMarkTagCard/index.tsx
@@ -18,7 +18,7 @@ export const ConnectFirstMarkTagCard: React.FC<ConnectFirstMarkTagCardPropType> 
       className={`absolute ${isActive ? '' : 'top-8 left-[calc(0%+32px)] z-10 w-[calc(100%-64px)]'} flex-row rounded-[20px] border border-white backdrop-blur bg-white/48 p-[39px] shadow-lg`}
     >
       <Stack gap={40}>
-        <Stack gap={4}>
+        <Stack gap={4} className='max-w-[360px] w-full'>
           <h4 className='font-semibold text-xl leading-[30px] text-gray-950'>Connect MarkTag </h4>
           <p className='font-normal text-sm text-gray-600'>
             Track your first party customer marketing data with cutting-edge precision.{' '}
