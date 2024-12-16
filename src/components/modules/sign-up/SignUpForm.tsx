63a9d7defa1437a9e56c76ab24a844fd8a531356
Simanta Paul <simanta.paul.26@gmail.com>
2024-12-17 03:03:50 +0600

    feat: add password validation

--- src/components/modules/sign-up/SignUpForm.tsx
+++ src/components/modules/sign-up/SignUpForm.tsx
@@ -22,7 +22,12 @@ export const SignUpForm = ({ setIsSignedUp }: { setIsSignedUp: (value: boolean) 
     validate: {
       name: (value) => (value.length === 0 ? 'Name is required' : null),
       email: (value) => (value.length === 0 ? 'Email is required' : null),
-      password: (value) => (value.length === 0 ? 'Password is required' : null),
+      password: (value) => {
+        if (value.length === 0) return 'Password is required';
+        if (value.length < 8) return 'Password must be at least 8 characters';
+        if (!/[A-Z]/.test(value)) return 'Password must contain at least 1 capital letter';
+        return null;
+      },
     },
   });
 
@@ -110,7 +115,9 @@ export const SignUpForm = ({ setIsSignedUp }: { setIsSignedUp: (value: boolean) 
             <PasswordInput
               label='Password'
               placeholder='Set password'
-              description='Password must contain at least 8 characters.'
+              description={
+                form.errors.password ? null : 'Password must contain at least 8 characters.'
+              }
               {...form.getInputProps('password')}
             />
           </motion.div>
