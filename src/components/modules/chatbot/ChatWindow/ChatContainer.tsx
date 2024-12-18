1756d9a42d003bf0f77ce88737702fe5a29cd9f9
Nazmul Haque <bpinazmul18@gmail.com>
2024-12-18 14:05:58 +0600

    Added: Chatbot message input

--- src/components/modules/chatbot/ChatWindow/ChatContainer.tsx
+++ src/components/modules/chatbot/ChatWindow/ChatContainer.tsx
@@ -1,12 +1,16 @@
 import { ContentDivider } from 'components/modules/chatbot/ChatWindow/ContentDivider.tsx';
+import { MessageInput } from 'components/modules/chatbot/ChatWindow/MessageInput.tsx';
 import { MessageSection } from 'components/modules/chatbot/ChatWindow/MessageSection.tsx';
 import React from 'react';
 
 export const ChatContainer: React.FC = () => {
   return (
-    <div className='w-full p-6 bg-white flex flex-col gap-8'>
-      <ContentDivider />
-      <MessageSection />
-    </div>
+    <>
+      <div className='w-full p-6 bg-white flex flex-col gap-8 overflow-y-scroll h-[564px]'>
+        <ContentDivider />
+        <MessageSection />
+      </div>
+      <MessageInput />
+    </>
   );
 };
--- src/components/modules/chatbot/ChatWindow/MessageSection.tsx
+++ src/components/modules/chatbot/ChatWindow/MessageSection.tsx
@@ -12,6 +12,14 @@ export const MessageSection: React.FC = () => {
       <ChatMessage message='I’d like to learn more' />
       <ChatMessage message='I’m a customer with a question.' />
       <ChatMessage isNabiq isBotTexting message='...' />
+      <ChatMessage
+        isNabiq
+        message='Ahoy! Thanks for your interest in John’s company. How can we help you today?'
+      />
+      <ChatMessage tappedMessage message='I’m a customer with a question.' />
+      <ChatMessage message='Sales team' />
+      <ChatMessage message='Support team' />
+      <ChatMessage isDeliver isStartAgain message='Start again' />
     </div>
   );
 };
