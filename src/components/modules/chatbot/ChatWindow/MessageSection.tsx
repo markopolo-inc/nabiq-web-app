7259e577465cff24b1db2788ba181702b4745fe2
Nazmul Haque <bpinazmul18@gmail.com>
2024-12-18 08:18:22 +0600

    Added: Chatbot message content

--- src/components/modules/chatbot/ChatWindow/MessageSection.tsx
+++ src/components/modules/chatbot/ChatWindow/MessageSection.tsx
@@ -4,10 +4,14 @@ import React from 'react';
 export const MessageSection: React.FC = () => {
   return (
     <div className='flex flex-col gap-4'>
-      <ChatMessage />
-      <ChatMessage />
-      <ChatMessage />
-      <ChatMessage />
+      <ChatMessage
+        isNabiq
+        message='Ahoy! Thanks for your interest in John’s company. How can we help you today?'
+      />
+      <ChatMessage message='I’m just browsing' />
+      <ChatMessage message='I’d like to learn more' />
+      <ChatMessage message='I’m a customer with a question.' />
+      <ChatMessage isNabiq isBotTexting message='...' />
     </div>
   );
 };
--- src/components/modules/chatbot/ChatWindow/index.tsx
+++ src/components/modules/chatbot/ChatWindow/index.tsx
@@ -8,7 +8,7 @@ type ChatWindowProps = {
 
 export const ChatWindow: React.FC<ChatWindowProps> = ({ onClick }) => {
   return (
-    <div className='absolute bottom-28 right-10 max-w-[400px] w-full bg-primary-600 flex flex-col items-center justify-center hover:cursor-pointer z-[999] rounded-3xl overflow-hidden shadow-2xl'>
+    <div className='absolute bottom-28 right-10 max-w-[400px] w-full bg-primary-600 flex flex-col items-center justify-center z-[999] rounded-3xl overflow-hidden shadow-2xl'>
       <ChatHeader onClick={onClick} />
       <ChatContainer />
     </div>
