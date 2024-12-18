f4f826b19220a085671d41e4e0d65fa2fd13b6fc
Simanta Paul <simanta.paul.26@gmail.com>
2024-12-18 17:51:43 +0600

    refactor: merge ecommerce

--- src/components/modules/integrations/integration-tabs/data-sources/index.ts
+++ src/components/modules/integrations/integration-tabs/data-sources/index.ts
@@ -1,5 +1,6 @@
 /*** Data Sources ***/
 export { Marktag } from 'src/components/modules/integrations/integration-tabs/data-sources/Marktag';
+export { ECommerce } from 'src/components/modules/integrations/integration-tabs/data-sources/ECommerce';
 
 /*** Modals ***/
 export { MarktagCreationsModals } from 'src/components/modules/integrations/integration-tabs/data-sources/modals/MarktagCreationsModals/MarktagConnectModal';
