cc3192154b0186c141dc3128b4bf4899ec7cebe4
Simanta Paul <simanta.paul.26@gmail.com>
2024-12-30 13:57:31 +0600

    feat: switch language button

--- src/components/modules/navigation/SwitchLanguage.tsx
+++ src/components/modules/navigation/SwitchLanguage.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@nabiq-ui';
 import { useTranslation } from 'react-i18next';
 
 export const SwitchLanguage = () => {
   const { i18n } = useTranslation();
   return (
-    <Button onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'ar' : 'en')}>
+    <button
+      onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'ar' : 'en')}
+      className='text-white'
+    >
       Switch Language
-    </Button>
+    </button>
   );
 };
