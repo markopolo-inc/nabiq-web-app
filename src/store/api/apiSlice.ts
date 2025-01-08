0756df86bf389d147014c4720e916f0c6ed1e7f5
Simanta Paul <simanta.paul.26@gmail.com>
2025-01-09 01:42:58 +0600

    feat: add custom base query

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -1,19 +1,56 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
+import toast from 'react-hot-toast';
 import { REHYDRATE } from 'redux-persist';
 import { getAuthToken } from 'src/utils/auth';
 
+import { logout } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BASE_API_URL,
+  prepareHeaders: async (headers) => {
+    // Set a default header
+    headers.set('Authorization', `Bearer ${await getAuthToken()}`);
+    headers.set('Content-Type', 'application/json');
+    // Add more headers as needed
+    return headers;
+  },
+});
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions,
+) => {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    const id = toast.loading('Logging out...');
+
+    setTimeout(() => {
+      api.dispatch(logout());
+      toast.dismiss(id);
+    }, 1000);
+  }
+  return result;
+};
+
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_API_URL,
-    prepareHeaders: async (headers) => {
-      // Set a default header
-      headers.set('Authorization', `Bearer ${await getAuthToken()}`);
-      headers.set('Content-Type', 'application/json');
-      // Add more headers as needed
-      return headers;
-    },
-  }),
+  // baseQuery: fetchBaseQuery({
+  //   baseUrl: import.meta.env.VITE_BASE_API_URL,
+  //   prepareHeaders: async (headers) => {
+  //     // Set a default header
+  //     headers.set('Authorization', `Bearer ${await getAuthToken()}`);
+  //     headers.set('Content-Type', 'application/json');
+  //     // Add more headers as needed
+  //     return headers;
+  //   },
+  // }),
+  baseQuery: baseQueryWithReauth,
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === REHYDRATE && reducerPath) {
       return action.payload?.[reducerPath];
