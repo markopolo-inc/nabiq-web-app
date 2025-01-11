fc130450148f07653bc4571aa60e0805cc7d2b88
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-11 23:30:15 +0600

    Added: translations in Monitoring page Header

--- src/components/modules/monitoring/header/MonitoringHeader.tsx
+++ src/components/modules/monitoring/header/MonitoringHeader.tsx
@@ -1,13 +1,13 @@
 import { Stack } from '@nabiq-ui';
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const MonitoringHeader: FC = () => {
+  const { t } = useTranslation();
   return (
     <Stack gap={4}>
-      <p className='text-gray-900 font-semibold text-3xl'>Monitoring</p>
-      <p className='text-gray-600 font-normal text-base'>
-        Know the performance of your campaigns in detail.
-      </p>
+      <p className='text-gray-900 font-semibold text-3xl'>{t('monitoring.title')}</p>
+      <p className='text-gray-600 font-normal text-base'>{t('monitoring.description')}</p>
     </Stack>
   );
 };
