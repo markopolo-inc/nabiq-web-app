822f7d82ae9677f869b9cc2b1aac043d7444ac0c
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-19 15:52:20 +0600

    Added: Translation in Acquisition chatbot

--- src/pages/AcquisitionChatbotPage/index.tsx
+++ src/pages/AcquisitionChatbotPage/index.tsx
@@ -1,9 +1,11 @@
 import { Stack } from '@nabiq-ui';
+import { useTranslation } from 'react-i18next';
 import { IntegrateChatbot } from 'src/components/modules/acquisition-chatbot';
 import { useGetAcquisitionApiKeyQuery } from 'src/store/acquisition/acquisition.ts';
 import { useAppSelector } from 'src/store/hooks.ts';
 
 const AcquisitionChatbotPage = () => {
+  const { t } = useTranslation();
   const { resourceId: brandId } = useAppSelector((state) => state.brand);
 
   const { data } = useGetAcquisitionApiKeyQuery(brandId);
@@ -12,9 +14,11 @@ const AcquisitionChatbotPage = () => {
       <Stack gap={64} align='center'>
         <Stack className='w-full items-start flex-row' justify='space-between'>
           <Stack gap={4}>
-            <p className='text-gray-900 text-3xl font-semibold'>Acquisition chatbot</p>
+            <p className='text-gray-900 text-3xl font-semibold'>
+              {t('acquisition_chatbot_page.acquisition_chatbot')}
+            </p>
             <p className='text-gray-600 text-base font-normal'>
-              Convert visitors into leads with the acquisition chatbot.
+              {t('acquisition_chatbot_page.convert_visitors')}
             </p>
           </Stack>
           {/*<Button variant='link' leadingIcon={<FiHelpCircle size={20} />}>*/}
