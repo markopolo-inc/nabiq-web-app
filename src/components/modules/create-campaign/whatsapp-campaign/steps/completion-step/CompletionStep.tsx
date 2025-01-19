c01f59ba1ec2343e4fb24f00d568cf625edfe84d
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-19 13:10:55 +0600

    Added: Translation in Whatsapp complete step

--- src/components/modules/create-campaign/whatsapp-campaign/steps/completion-step/CompletionStep.tsx
+++ src/components/modules/create-campaign/whatsapp-campaign/steps/completion-step/CompletionStep.tsx
@@ -1,7 +1,9 @@
 import { FiCheck } from '@nabiq-icons';
 import { Stack } from '@nabiq-ui';
+import { useTranslation } from 'react-i18next';
 
 export const CompletionStep = () => {
+  const { t } = useTranslation();
   return (
     <Stack className='w-full' align='center'>
       <Stack gap={64} maw={744} className='p-8 border border-gray-200 rounded-xl shadow-lg'>
@@ -9,10 +11,11 @@ export const CompletionStep = () => {
           <div className='h-8 w-8 rounded-full bg-success-600 flex items-center justify-center'>
             <FiCheck size={18} color='white' />
           </div>
-          <p className='text-gray-900 text-lg font-semibold'>Captain Nabiq is now sailing!</p>
+          <p className='text-gray-900 text-lg font-semibold'>
+            {t('create_campaign.captain_nabiq_sailing')}
+          </p>
           <p className='text-gray-600 text-sm font-normal'>
-            Captain Nabiq is working on your leads. Want to add more? Upload them by integraating
-            with more datasources.
+            {t('create_campaign.captain_nabiq_working')}
           </p>
         </Stack>
       </Stack>
