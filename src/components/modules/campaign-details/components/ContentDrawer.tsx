fa33189182ad40e5e1596e633480e2e1aad0e4f6
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-19 11:06:07 +0600

    Added: Translation in Campaign details content drawer

--- src/components/modules/campaign-details/components/ContentDrawer.tsx
+++ src/components/modules/campaign-details/components/ContentDrawer.tsx
@@ -2,6 +2,7 @@ import { FiDot } from '@nabiq-icons';
 import { Badge, Drawer, DrawerBody, DrawerHeader, GatewayLogo, Group } from '@nabiq-ui';
 import { capitalize } from 'lodash';
 import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 
 const ContentDrawer = ({
   opened,
@@ -12,6 +13,7 @@ const ContentDrawer = ({
   onClose: () => void;
   selectedContent: any;
 }) => {
+  const { t } = useTranslation();
   return (
     <Drawer
       opened={opened}
@@ -24,11 +26,15 @@ const ContentDrawer = ({
       <DrawerHeader>
         <Group>
           <p className='text-gray-900 text-lg font-semibold'>{selectedContent?.email}</p>
-          <Badge color='gray'>Step {selectedContent?.step}</Badge>
+          <Badge color='gray'>
+            {t('campaign_details.step')} {selectedContent?.step}
+          </Badge>
         </Group>
 
         <p className='text-gray-600 text-sm font-medium'>
-          Sent on {moment(selectedContent?.sentOn).format('MMM D, YYYY')} at{' '}
+          {t('campaign_details.sent_on', {
+            time: moment(selectedContent?.sentOn).format('MMM D, YYYY'),
+          })}
           {moment(selectedContent?.sentOn).format('h:mm a')}
         </p>
       </DrawerHeader>
@@ -38,12 +44,14 @@ const ContentDrawer = ({
             <Group>
               <GatewayLogo app={selectedContent?.platform} width={32} />
               <p className='text-gray-900 text-lg font-semibold'>
-                {capitalize(selectedContent?.channel || 'Email')}
+                {capitalize(selectedContent?.channel || t('home_page.channel_email'))}
               </p>
             </Group>
             <Badge color='success'>
               <FiDot size={8} />
-              Sent on {moment(selectedContent?.sentOn).format('MMM D, YYYY')} at{' '}
+              {t('campaign_details.sent_on', {
+                time: moment(selectedContent?.sentOn).format('MMM D, YYYY'),
+              })}
               {moment(selectedContent?.sentOn).format('h:mm a')}
             </Badge>
           </Group>
