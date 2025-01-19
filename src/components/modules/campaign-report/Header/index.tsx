89f0ef4328e913cff2ebd38360b3631dd102c1d9
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-19 11:19:41 +0600

    Added: Translation in Campaign reports page

--- src/components/modules/campaign-report/Header/index.tsx
+++ src/components/modules/campaign-report/Header/index.tsx
@@ -15,14 +15,14 @@ export const CampaignReportHeader = () => {
       <Group justify='space-between'>
         <Stack gap={4}>
           <p className='text-gray-900 text-[20px] font-semibold'>
-            Tropical Treasures: {capitalize(name?.split('-').join(' '))}
+            {t('campaign_report.tropical_treasures')}: {capitalize(name?.split('-').join(' '))}
           </p>
           <p className='text-gray-600 text-base font-normal'>
             {t('campaign_details.list_description')}
           </p>
         </Stack>
         <Button size='md' variant='secondary' onClick={() => navigate(-1)}>
-          Back to campaigns
+          {t('campaign_report.back_to_campaigns')}
         </Button>
       </Group>
     </>
