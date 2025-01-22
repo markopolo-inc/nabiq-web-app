4576075f95b836baddbcc02bcbfd9f0f1d35231b
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-22 11:34:46 +0600

    Added: Translation in Connect Marktag page

--- src/pages/ConnectMarktagPage/index.tsx
+++ src/pages/ConnectMarktagPage/index.tsx
@@ -1,10 +1,12 @@
 import { HeaderTitle } from 'layouts';
+import { useTranslation } from 'react-i18next';
 import { MarktagDetails, MarktagSidebar } from 'src/components/modules/home';
 
 const ConnectMarktag = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <HeaderTitle>Nabiq | Connect Marktag</HeaderTitle>
+      <HeaderTitle>{t('page_title.connect_marktag_title')}</HeaderTitle>
 
       <div className='min-h-screen mx-auto max-w-full lg:grid lg:grid-cols-12 lg:gap-x-8'>
         <MarktagSidebar />
