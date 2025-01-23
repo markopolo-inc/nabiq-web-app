4af6a7c0be33b4e6d60b2d1db321a057d6e848a0
Simanta Paul <simanta.paul.26@gmail.com>
2025-01-23 11:31:26 +0600

    feat: add language button in onboarding

--- src/components/UI/components/Topbar/index.tsx
+++ src/components/UI/components/Topbar/index.tsx
@@ -1,6 +1,7 @@
 import { Image } from '@mantine/core';
 import { motion } from 'framer-motion';
 import NabiqLogo from 'src/assets/logo/nabiq-dark-logo.png';
+import { SwitchLanguage } from 'src/components/modules/navigation';
 
 export const Topbar = () => {
   return (
@@ -8,9 +9,13 @@ export const Topbar = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className={`fixed top-0 left-0 w-screen bg-gray-950 h-[56px] flex flex-col justify-center items-center z-10`}
+      className={`fixed top-0 left-0 w-screen bg-gray-950 h-[56px] flex justify-between items-center px-[48px] z-10`}
     >
-      <Image src={NabiqLogo} alt='Nabiq' className='w-[70px]' />
+      <div>
+        <Image src={NabiqLogo} alt='Nabiq' className='w-[70px]' />
+      </div>
+
+      <SwitchLanguage />
     </motion.div>
   );
 };
--- src/layouts/OnboardingLayout.tsx
+++ src/layouts/OnboardingLayout.tsx
@@ -15,14 +15,6 @@ export const OnboardingLayout = ({
 }) => {
   return (
     <div className='flex flex-col h-screen w-screen'>
-      {/* <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className={`fixed top-0 left-0 w-screen bg-gray-950 h-[56px] flex flex-col justify-center items-center z-10`}
-      >
-        <Image src={NabiqLogo} alt='Nabiq' className='w-[70px]' />
-      </motion.div> */}
       <div
         className={`grid grid-cols-1 lg:grid-cols-[43%_57%] flex-1 mt-[56px] h-[calc(100vh-56px)]`}
       >
