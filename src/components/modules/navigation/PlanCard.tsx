e4ac837cb152ddbf40fba38c81ebee94c2bb8df6
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-26 17:16:32 +0600

    Added: Plan card translation

--- src/components/modules/navigation/PlanCard.tsx
+++ src/components/modules/navigation/PlanCard.tsx
@@ -1,16 +1,16 @@
 import { Button, Progress, Stack } from '@nabiq-ui';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 export const PlanCard = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
   const target = 30;
   return (
     <Stack className='px-4 py-5 bg-gray-50 rounded-lg' gap={16}>
       <Stack gap={8}>
-        <p className='text-xs text-gray-900 font-semibold'>Used MAU</p>
-        <p className='text-xs text-gray-600'>
-          You have targeted {target}% of your available monthly active users.
-        </p>
+        <p className='text-xs text-gray-900 font-semibold'>{t('plan_card.used_mau')}</p>
+        <p className='text-xs text-gray-600'>{t('plan_card.target_notification', { target })}</p>
       </Stack>
 
       <Progress
@@ -25,7 +25,7 @@ export const PlanCard = () => {
         value={target}
       />
       <Button variant='link' onClick={() => navigate('/billing')}>
-        Upgrade Plan
+        {t('plan_card.plan_upgrade')}
       </Button>
     </Stack>
   );
