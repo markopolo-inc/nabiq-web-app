e59f0bc1d340b7f2fd6eef11bb2029959e6f786f
Nazmul Haque <bpinazmul18@gmail.com>
2025-01-29 13:42:06 +0600

    Added: Conditionally render screen and translations

--- src/components/modules/reset-pass/CheckEmail.tsx
+++ src/components/modules/reset-pass/CheckEmail.tsx
@@ -1,7 +1,16 @@
 import { FiMail01 } from '@nabiq-icons';
 import { Button, Stack } from '@nabiq-ui';
+import { useTranslation } from 'react-i18next';
 
-export const CheckEmail = () => {
+type CheckEmailProps = {
+  onSetup: () => void;
+};
+
+export const CheckEmail = ({ onSetup }: CheckEmailProps) => {
+  const { t } = useTranslation();
+  const resendEmail = () => {
+    onSetup();
+  };
   return (
     <>
       <div className='p-3.5 bg-gray-100 rounded-full'>
@@ -9,14 +18,16 @@ export const CheckEmail = () => {
       </div>
 
       <Stack align='center' gap={4}>
-        <p className='text-gray-900 text-2xl font-semibold'>Please check your email </p>
+        <p className='text-gray-900 text-2xl font-semibold'>
+          {t('reset_pass.please_check_your_email')}
+        </p>
         <p className='text-gray-500 text-xl leading-[30px] font-normal text-center'>
-          We have sent you instructions to reset your password on johndoe@gmail.com
+          {t('reset_pass.reset_password_instructions', { email: 'example@gmai.com' })}
         </p>
       </Stack>
 
-      <Button type='submit' fullWidth>
-        Resend email
+      <Button type='submit' fullWidth onClick={resendEmail}>
+        {t('reset_pass.resend_email')}
       </Button>
     </>
   );
---
+++ src/interfaces/ResetPassStep.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+export enum ResetPasswordStep {
+  EMAIL_INPUT,
+  RESEND_CODE,
+  NEW_PASSWORD,
+  SUCCESS,
+}
